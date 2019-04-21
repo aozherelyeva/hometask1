220e3cfba10adac79d354723038c0d184c9ff546
aozherelyeva <aozherelyeva@gmail.com>
2019-04-21 13:26:36 +0300

    Resolve task0 and task1

--- src/main/java/com/javalesson/chapter1/task0/LoopTask.java
+++ src/main/java/com/javalesson/chapter1/task0/LoopTask.java
@@ -1,6 +1,8 @@
 package com.javalesson.chapter1.task0;
 
 
+import java.util.Scanner;
+
 /**
  * Создайте программу, которая будет принимать две группы параметров:
  * - количество циклов выполнения программы;
@@ -8,28 +10,49 @@ package com.javalesson.chapter1.task0;
  * и выводить на экран последовательность результатов вычисления следующего выражения
  * (a + 2^0*b),(a + 2^0*b + 2^1*b),(a + 2^0*b + 2^1*b + 2^2*b)..., (a+2^0*b + 2^1*b + 2^2*b +...+ 2^(n-1)*b)
  * где ^ обозначает возведение в степень, и эта операция выполняется до умножения.
- *
+ * <p>
  * Пример:
- *  Please enter the number of iterations
- *   2
- *  Enter the group of 3 numbers
- *   0 2 10
- *  Output:
- *   2 6 14 30 62 126 254 510 1022 2046
- *  Enter the group of 3 numbers
- *   5 3 5
- *  Output:
- *  8 14 26 50 98
- *
- *  Пример:
- *  a=5, b=3, n=5
- *  (5+2^0*3)=8,(5+2^0*3 + 2^1*3)=14,(5+2^0*3 + 2^1*3 + 2^2*3)=26,(5+2^0*3 + 2^1*3 + 2^2*3 + 2^3*3)=50,
- *  (5+2^0*3 + 2^1*3 + 2^2*3 + 2^3*3 + 2^4*3)=98
- *
+ * Please enter the number of iterations
+ * 2
+ * Enter the group of 3 numbers
+ * 0 2 10
+ * Output:
+ * 2 6 14 30 62 126 254 510 1022 2046
+ * Enter the group of 3 numbers
+ * 5 3 5
+ * Output:
+ * 8 14 26 50 98
+ * <p>
+ * Пример:
+ * a=5, b=3, n=5
+ * (5+2^0*3)=8,(5+2^0*3 + 2^1*3)=14,(5+2^0*3 + 2^1*3 + 2^2*3)=26,(5+2^0*3 + 2^1*3 + 2^2*3 + 2^3*3)=50,
+ * (5+2^0*3 + 2^1*3 + 2^2*3 + 2^3*3 + 2^4*3)=98
  */
 public class LoopTask {
 
     public static void main(String[] args) {
-        
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a number of iterations");
+        int iterations = in.nextInt();
+
+        for (int i = 0; i <= iterations; i++) {
+            System.out.println("Enter the group of 3 numbers");
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int accumulator = a;
+            for (int j = 0; j < n; j++) {
+                int x = (int) Math.pow(2, j);
+                accumulator = accumulator + x * b;
+                System.out.print(accumulator + " ");
+            }
+            System.out.println();
+            break;
+        }
+        in.close();
+
     }
+
 }
--- src/main/java/com/javalesson/chapter1/task1/TriangleTask.java
+++ src/main/java/com/javalesson/chapter1/task1/TriangleTask.java
@@ -1,14 +1,36 @@
 package com.javalesson.chapter1.task1;
+
+import java.util.Scanner;
+
 /**
- *  Напишите программу которая принимает 3 числовых значения и проверяет, могут ли они быть
- *  сторонами треугольника.
- *  Для ввода значений с консоли Вам понадобится класс Scanner.
- *  <code>Scanner scanner = new Scanner(System.in);</code>
- *  <code>scanner.nextInt();</code>
+ * Напишите программу которая принимает 3 числовых значения и проверяет, могут ли они быть
+ * сторонами треугольника.
+ * Для ввода значений с консоли Вам понадобится класс Scanner.
+ * <code>Scanner scanner = new Scanner(System.in);</code>
+ * <code>scanner.nextInt();</code>
  */
 public class TriangleTask {
 
     public static void main(String[] args) {
+        Scanner in2 = new Scanner(System.in);
+        System.out.println("Please enter the first side");
+        int sideOne = in2.nextInt();
+
+        Scanner in3 = new Scanner(System.in);
+        System.out.println("Please enter the second side");
+        int sideTwo = in3.nextInt();
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the third side");
+        int sideThree = in.nextInt();
+
+        if ((sideOne + sideTwo) > sideThree
+                && (sideOne + sideThree) > sideTwo
+                && (sideTwo + sideThree) > sideOne) {
+            System.out.println("These sides can form a valid triangle!");
+        } else {
+            System.out.println("These sides can not form the triangle :(");
+        }
 
     }
 }
